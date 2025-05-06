4fceb97a5893111c97079b1f170d915c74d91662
mikaelnyawanda <bolomycke65@gmail.com>
2025-05-06 07:11:47 -0700

    0x01. NoSQL

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,19 +1,12 @@
-## 10-update_topics.py
+#!/usr/bin/env python3
+'''Task 10: Change school topics
+'''
+
 
 def update_topics(mongo_collection, name, topics):
-    """
-    Update the topics of a school document.
-
-    Parameters:
-    mongo_collection -- the pymongo collection object
-    name -- the name of the school to update
-    topics -- the new list of topics to set
-
-    Returns:
-    The number of documents modified.
-    """
-    result = mongo_collection.update_one(
-        {"name": name},       ## filter to find the document
-        {"$set": {"topics": topics}}  ## update operation
+    '''Changes all topics of a collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
     )
-    return result.modified_count
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,15 +1,10 @@
-## 9-insert_school.py
+#!/usr/bin/env python3
+'''Task 9: Insert a document in Python
+'''
+
 
 def insert_school(mongo_collection, **kwargs):
-    """
-    Inserts a new document in a collection with the given keyword arguments.
-
-    Parameters:
-    mongo_collection -- the pymongo collection object
-    **kwargs -- the document attributes to insert
-
-    Returns:
-    The new _id of the inserted document.
-    """
+    '''Inserts a new document in a collection.
+    '''
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
